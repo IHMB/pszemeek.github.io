4a4807d03526a67445268dc139a81038cf4943ca
IHMB <32036041+IHMB@users.noreply.github.com>
2019-10-25 18:21:05 +0200

    Update kalkulator.php

--- kalkulator.php
+++ kalkulator.php
@@ -1,3 +1,15 @@
 <?php
-  echo "string";
- ?>
+	if(isset($_GET['continent'])){
+		$continent=$_GET['continent'];
+		$country=$_GET['country'];
+		$ile=$_GET['ile'];
+		$conn=new mysqli("localhost","id11347156_safearthh","jD9ajF-yvC93k","id11347156_data");
+		if($conn->connect_error){
+			echo "Connection failed: ".$conn->connect_error;
+		}
+		$sql="SELECT kraje.Populacja, kraje.Wygenerowane_odpady_tony FROM kraje LEFT JOIN kontynenty ON kraje.ID_Kontynentu=kontynenty.ID_Kontynentu WHERE kontynenty.Kontynent='Europa' AND kraje.Nazwa_kraju='Polska'";
+		$result=$conn->query($sql);
+		$row=$result->fetch_assoc();
+		echo(($row['Wygenerowane_odpady_tony']/$row['Populacja'])*$ile);
+	}
+?>
